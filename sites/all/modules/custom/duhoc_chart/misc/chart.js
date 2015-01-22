25c99f304b452903d1f1cb11284d84c03b35cb1f
mr.vantri <trichu@luxcer.vom>
2015-01-22 17:52:52 +0700

    addd

--- sites/all/modules/custom/duhoc_chart/misc/chart.js
+++ sites/all/modules/custom/duhoc_chart/misc/chart.js
@@ -2,6 +2,7 @@ jQuery(document).ready(function() {
     jQuery(".chart_sale select").change(function() {
         var id = jQuery(this).val();
         jQuery('#chart_div').text('loading...').css('color','red');
+        jQuery('.content_mont').html('');
         jQuery.ajax({
             url: 'https://www.google.com/jsapi?callback',
             cache: true,
@@ -9,21 +10,27 @@ jQuery(document).ready(function() {
             success: function(){
                 google.load('visualization', '1', {packages:['corechart'], 'callback' : function()
                 {
-
                     jQuery.ajax({
                         type: "POST",
                         dataType: "json",
                         url: 'manage/'+id,
                         success: function(output) {
-                            console.log(output.data);
-                            var data = google.visualization.arrayToDataTable([ ["Month", "Now year", "Tagets"], ["January",      4,      8]]);
-                            var options = {title: 'My Daily Activities'};
+                            var chuan = '['+output.data+']';
+                            var row = jQuery.parseJSON(chuan);
+                            var data = new google.visualization.DataTable();
+                            data.addColumn('string', '');
+                            data.addColumn('number', 'Now year');
+                            data.addColumn('number', 'Taget');
+                            data.addRows(row);
+                            var options = {
+                                chartArea:{left:20,top:60,width:"86%"},
+                                hAxis: {title: "", titleTextStyle: {color: "red"}}
+                            };
                             var chart = new google.visualization.ColumnChart(document.getElementById('chart_div'));
                             chart.draw(data, options);
+
                         }
                     });
-
-
                 }
                 });
                 return true;
