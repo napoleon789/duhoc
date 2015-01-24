9ff45e05e14875778ad5c22161ba0264916b2c60
mr.vantri <trichu@luxcer.vom>
2015-01-24 10:58:06 +0700

    add

--- sites/all/modules/custom/duhoc_chart/misc/chart.js
+++ sites/all/modules/custom/duhoc_chart/misc/chart.js
@@ -13,12 +13,10 @@ jQuery(document).ready(function() {
                     jQuery.ajax({
                         type: "POST",
                         dataType: "json",
-                        url: 'manage/'+id,
+                        url: 'sales/'+id,
                         success: function(output) {
                             var right = output.right;
-                            console.log(right);
                             jQuery('.content_mont').html(right);
-
                             var chuan = '['+output.data+']';
                             var row = jQuery.parseJSON(chuan);
                             var data = new google.visualization.DataTable();
@@ -32,7 +30,46 @@ jQuery(document).ready(function() {
                             };
                             var chart = new google.visualization.ColumnChart(document.getElementById('chart_div'));
                             chart.draw(data, options);
-
+                        }
+                    });
+                }
+                });
+                return true;
+            }
+        });
+    });
+    //Chart revenue
+    jQuery(".chart_reve select").change(function() {
+        var id = jQuery(this).val();
+        jQuery('#chart_div2').text('loading...').css('color','red');
+        jQuery('.content_revenue').html('');
+        jQuery.ajax({
+            url: 'https://www.google.com/jsapi?callback',
+            cache: true,
+            dataType: 'script',
+            success: function(){
+                google.load('visualization', '1', {packages:['corechart'], 'callback' : function()
+                {
+                    jQuery.ajax({
+                        type: "POST",
+                        dataType: "json",
+                        url: 'revenue/'+id,
+                        success: function(output) {
+                            var right = output.right;
+                            jQuery('.content_revenue').html(right);
+                            var chuan = '['+output.data+']';
+                            console.log(chuan);
+                            var row = jQuery.parseJSON(chuan);
+                            var data = new google.visualization.DataTable();
+                            data.addColumn('string', '');
+                            data.addColumn('number', 'Now year');
+                            data.addRows(row);
+                            var options = {
+                                chartArea:{left:20,top:60,width:"86%"},
+                                hAxis: {title: "", titleTextStyle: {color: "red"}}
+                            };
+                            var chart = new google.visualization.ColumnChart(document.getElementById('chart_div2'));
+                            chart.draw(data, options);
                         }
                     });
                 }
